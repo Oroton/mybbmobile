02fc9c3a654cf871090fce0b6fcbd38593d2d8ba
Booher <jake.booher@gmail.com>
2013-11-11 12:07:38 +0100

    Fixed compatibility issues with MyBB 1.6.11

--- inc/languages/english/mybbmobile.lang.php
+++ inc/languages/english/mybbmobile.lang.php
@@ -6,28 +6,6 @@
 * Based off of MyBB GoMobile
 */
 
-// ACP Strings
-$l['mybbmobile'] = "MyBBMobile";
-$l['mybbmobile_desc'] = "Allow your members to view your forum more easily from their mobile device.";
-
-// Setting Strings
-$l['mybbmobile_settings_mobile_name_title'] = "Mobile Board Name";
-$l['mybbmobile_settings_mobile_name'] = "Use this setting to shorten both your header and breadcrumb (navigation) text. You may use full HTML formatting.";
-$l['mybbmobile_settings_redirect_enabled_title'] = "Enable Portal Redirect?";
-$l['mybbmobile_settings_redirect_enabled'] = "Enable or disable the MyBBMobile portal redirect. Enabling this will automatically redirect users who visit to portal on their mobile, to a page of your choice instead.";
-$l['mybbmobile_settings_redirect_location_title'] = "Redirect Location";
-$l['mybbmobile_settings_redirect_location'] = "Enter the location (relative) that you would like to redirect users to. Ignore this if the redirect option above is not enabled.";
-$l['mybbmobile_settings_theme_id_title'] = "Theme ID";
-$l['mybbmobile_settings_theme_id'] = "Enter the tid (Theme ID) of MyBBMobile below. This is used to switch the user to the mobile version.";
-$l['mybbmobile_settings_permstoggle_title'] = "Use theme permissions?";
-$l['mybbmobile_settings_permstoggle'] = "Enable this setting to use the theme-specific usage permissions. If disabled, all groups will be able to use MyBBMobile. Selecting NO also saves a query, and is recommended unless you require theme permissions.";
-$l['mybbmobile_settings_homename_title'] = "Home Name";
-$l['mybbmobile_settings_homename'] = "This is the text that will appear in the footer of MyBBMobile in place of the home link. It is recommended that you keep this to as few charaters as possible.";
-$l['mybbmobile_settings_homelink_title'] = "Home Link";
-$l['mybbmobile_settings_homelink'] = "Below is the link that appears in the footer of MyBBMobile.";
-$l['mybbmobile_settings_strings_title'] = "User Agent Strings";
-$l['mybbmobile_settings_strings'] = "Separated by a new line, enter the strings you wish to check for and then send to the mobile theme. You should not normally need to adjust this.";
-
 // Misc. strings used through MyBBMobile
 $l['mybbmobile_mobile_version'] = "Mobile Version";
 $l['mybbmobile_switch_full'] = "Switch to Full Version";
@@ -47,7 +25,7 @@ $l['mybbmobile_post_warn'] = "Warn";
 $l['mybbmobile_post_pm'] = "PM";
 $l['mybbmobile_page'] = "Page:";
 $l['mybbmobile_of'] = "of";
-$l['mybbmobile_copyrights'] = "Powered by <a href='http://mybb.com' target='_blank'>MyBB</a>, mobile version by <a href='http://mybbgm.com' target='_blank'>MyBBMobile</a>.";
+$l['mybbmobile_copyrights'] = "Powered by <a href='http://mybb.com' target='_blank'>MyBB</a>, mobile version by <a href='http://mybbmobile.com' target='_blank'>MyBBMobile</a>.";
 $l['mybbmobile_showsig'] = "Display your Signature?";
 $l['mybbmobile_disablesmilies'] = "Disable smilies in this message?";
 $l['mybbmobile_savecopy'] = "Save a copy?";
